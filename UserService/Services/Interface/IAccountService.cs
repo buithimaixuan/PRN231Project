2350d24b35c82f0abece7bec81118dccc6285d5c
Tran Phuong Nam <namtpce170126@fpt.edu.vn>
2025-03-15 20:20:18 +0700

    Fix lỗi thiếu method

--- UserService/Services/Interface/IAccountService.cs
+++ UserService/Services/Interface/IAccountService.cs
@@ -1,6 +1,25 @@
-﻿namespace UserService.Services.Interface
+﻿using UserService.Models;
+
+namespace UserService.Services.Interface
 {
     public interface IAccountService
     {
+        Task<IEnumerable<Account>> GetListAllAccount();
+        Task<IEnumerable<Account>> GetListAccountByRoleId(int role_id);
+        Task<Account> GetByIdAccount(int id);
+
+        Task<Account> GetByUsername(string username);
+
+        Task<Account> GetByIdFacebook(string fbId);
+
+        Task AddAccount(Account item);
+
+        Task CreateNewFacebookAccount(string fbId, string name, string email, string avatar);
+        Task UpdateAccount(Account item);
+        Task DeleteAccount(Account item);
+        Task<Account?> GetAccountByEmail(string email);
+        Task<int> GetTotalFarmerService();
+        Task<string?> GetFullNameByUsername(string username);
+        Task<int> GetTotalExpertService();
     }
 }
