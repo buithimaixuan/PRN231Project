d8307ac13f734de43fbd1352dc99b5f72e665253
THUC <nht.071203@gmail.com>
2025-03-15 20:55:17 +0700

    add NewsAPIController, CRUD, FilterNews

---
+++ PostService/DTOs/NewsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace PostService.DTOs
+{
+    public class NewsDTO
+    {
+        public int CategoryNewsId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+    }
+
+}
--- PostService/Program.cs
+++ PostService/Program.cs
@@ -1,6 +1,21 @@
+using PostService.DAOs;
+using PostService.Repositories.Implement;
+using PostService.Repositories.Interface;
+using PostService.Services.Implement;
+using PostService.Services.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// News DI
+builder.Services.AddScoped<NewsDAO>();
+builder.Services.AddScoped<CategoryNewsDAO>();
+builder.Services.AddScoped<INewsRepository, NewsRepository>();
+builder.Services.AddScoped<ICategoryNewsRepository, CategoryNewsRepository>();
+builder.Services.AddScoped<INewsService, NewsService>();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
