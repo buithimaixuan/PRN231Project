3b91d95677ecea32fe047f219a529376ec00429a
Tran Phuong Nam <namtpce170126@fpt.edu.vn>
2025-03-15 21:52:16 +0700

    Add api for post and category post

---
+++ PostService/Controllers/PostCategories.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PostService.Models;
+using PostService.Services.Implement;
+using PostService.Services.Interface;
+
+namespace PostService.Controllers
+{
+    [Route("api/post-categories")]
+    [ApiController]
+    public class PostCategories : ControllerBase
+    {
+        private readonly ICategoryPostService _service;
+
+        public PostCategories(ICategoryPostService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _service.GetAllCategory();
+            if (response == null) return BadRequest("Error message: list category is null");
+
+            if (response.Count() <= 0) BadRequest("Error message: list category is empty");
+
+            return Ok(response);
+        }
+
+        [HttpGet("{categoryPostId}")]
+        public async Task<IActionResult> GetOne([FromRoute] int categoryPostId)
+        {
+            if (categoryPostId <= 0) return BadRequest("Error message: Category Id is not valid.");
+
+            var response = await _service.FindById(categoryPostId);
+
+            return (response != null) ? Ok(response) : NotFound("Output message: Cannot find category");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CategoryPost request)
+        {
+            if (request == null) return BadRequest("Error message: Request body is invalid");
+
+            var response = await _service.AddCatPost(request);
+
+            return (response > 0) ? Ok("Output message: Create new post successfully") : BadRequest("Output message: Create category post fail");
+        }
+
+        [HttpPut("{categoryPostId}")]
+        public async Task<IActionResult> Update([FromRoute] int categoryPostId, [FromBody] CategoryPost request)
+        {
+            if (categoryPostId <= 0) return BadRequest("Error message: Fail to get category post Id!");
+
+            if (request == null) return BadRequest("Error message: Fail to get Category post!");
+
+            if (categoryPostId != request.CategoryPostId) return NotFound("Error message: Category Post Id is not valid with request body!");
+
+            var response = await _service.UpdateCatPost(request);
+
+            if (response <= 0) return BadRequest("Output message: Fail to update category post!");
+
+            return Ok("Output message: Update successfully");
+        }
+
+        [HttpDelete("{categoryPostId}")] 
+        public async Task<IActionResult> Delete([FromRoute] int categoryPostId)
+        {
+            if (categoryPostId <= 0) return BadRequest("Error message: Fail to get Categort Post Id!");
+
+            int isSuccess = await _service.DeleteCatPost(categoryPostId);
+
+            return (isSuccess > 0) ? Ok("Output message: Delete successfully") : BadRequest("Output message: Fail to delete category post.");
+        }
+    }
+}
--- PostService/Program.cs
+++ PostService/Program.cs
@@ -1,6 +1,38 @@
+using PostService.DAOs;
+using PostService.Repositories.Implement;
+using PostService.Repositories.Interface;
+using PostService.Services.Implement;
+using PostService.Services.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<CategoryNewsDAO>();
+builder.Services.AddScoped<CategoryPostDAO>();
+builder.Services.AddScoped<CommentPostDAO>();
+builder.Services.AddScoped<LikePostDAO>();
+builder.Services.AddScoped<NewsDAO>();
+builder.Services.AddScoped<PostDAO>();
+builder.Services.AddScoped<PostImageDAO>();
+builder.Services.AddScoped<SharePostDAO>();
+builder.Services.AddScoped<ViewDAO>();
+
+builder.Services.AddScoped<ICategoryNewsRepository, CategoryNewsRepository>();
+builder.Services.AddScoped<ICategoryPostRepository, CategoryPostRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ILikePostRepository, LikePostRepository>();
+builder.Services.AddScoped<INewsRepository, NewsRepository>();
+builder.Services.AddScoped<IPostImageRepository, PostImageRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<ISharePostRepository, SharePostRepository>();
+builder.Services.AddScoped<IViewRepository, ViewRepository>();
+
+builder.Services.AddScoped<ICategoryPostService, CategoryPostService>();
+builder.Services.AddScoped<INewsService, NewsService>();
+builder.Services.AddScoped<IPostImageService, PostImageService>();
+builder.Services.AddScoped<IPostService, PostsService>();
+builder.Services.AddScoped<IViewService, ViewService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
