0483144f7e1d1c0bff740430c8a7f1b5ff62bfa0
tranphuongnamstudy <namtpce170126@fpt.edu.vn>
2025-03-21 13:46:30 +0700

    List post in home page

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
+﻿using Client.DTOs;
 using Client.Models;
 using Client.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using NuGet.Protocol.Plugins;
 using System.Diagnostics;
 using System.Net.Http.Headers;
 
@@ -13,6 +13,8 @@ namespace Client.Controllers
         private readonly HttpClient client;
         private string authenUrl = "";
         private string accountUrl = "";
+        private string postUrl = "";
+        private string categoryPostUrl = "";
 
         private readonly ILogger<HomeController> _logger;
 
@@ -23,6 +25,8 @@ namespace Client.Controllers
             client.DefaultRequestHeaders.Accept.Add(contentType);
             authenUrl = "https://localhost:7272/api/Auth";
             accountUrl = "https://localhost:7272/api/Accounts";
+            postUrl = "https://localhost:7231/api/post";
+            categoryPostUrl = "https://localhost:7231/api/post-categories";
             this._logger = logger;
         }
 
@@ -31,16 +35,62 @@ namespace Client.Controllers
             int accId = GetSessionAccId();
             var viewModel = new HomeViewModel();
 
+            // Tạo danh sách các task để gọi API song song
+            var tasks = new List<Task>();
+
+            //Lay account cua nguoi dung hien tai
             if (accId > 0)
             {
-                //Lay account tu accountId
-                var accountInfor = await client.GetAsync($"{accountUrl}/DetailFarmer{accId}");
-                var responseAccount = await accountInfor.Content.ReadAsStringAsync();
-                viewModel.Account = JsonConvert.DeserializeObject<Account>(responseAccount);
+                tasks.Add(Task.Run(async () =>
+                {
+                    var accountInfor = await client.GetAsync($"{accountUrl}/DetailFarmer{accId}");
+                    if (accountInfor.IsSuccessStatusCode)
+                    {
+                        var responseAccount = await accountInfor.Content.ReadAsStringAsync();
+                        viewModel.Account = JsonConvert.DeserializeObject<Account>(responseAccount);
+                    }
+                }));
             }
 
+            //Lấy danh sách bài viết từ PostService
+            var postTask = client.GetAsync($"{postUrl}/all/available");
+            tasks.Add(postTask);
+
+            //Lấy danh mục bài viết
+            var categoryTask = client.GetAsync($"{categoryPostUrl}");
+            tasks.Add(categoryTask);
+
+            // Chạy tất cả các task song song
+            await Task.WhenAll(tasks);
+
             //Xu ly lay list post o day, neu co
-            
+            var listPostDTO = await postTask;
+            if (listPostDTO.IsSuccessStatusCode)
+            {
+                var responseListPost = await listPostDTO.Content.ReadAsStringAsync();
+                viewModel.PostDTOs = JsonConvert.DeserializeObject<List<PostDTO>>(responseListPost);
+
+                // ✅ Gọi API AccountService để lấy thông tin Account của từng bài viết song song
+                var accountTasks = viewModel.PostDTOs.Select(async postDto =>
+                {
+                    var accountResponse = await client.GetAsync($"{accountUrl}/DetailFarmer{postDto.post.AccountId}");
+                    if (accountResponse.IsSuccessStatusCode)
+                    {
+                        var responseAccount = await accountResponse.Content.ReadAsStringAsync();
+                        postDto.Account = JsonConvert.DeserializeObject<Account>(responseAccount);
+                    }
+                });
+                await Task.WhenAll(accountTasks);
+            }
+
+            //Xu ly category post
+            var listCategoryPost = await categoryTask;
+            if (listCategoryPost.IsSuccessStatusCode)
+            {
+                var responseListCategoryPost = await listCategoryPost.Content.ReadAsStringAsync();
+                viewModel.categoryPosts = JsonConvert.DeserializeObject<List<CategoryPost>>(responseListCategoryPost);
+            }
+
             return View(viewModel);
         }
 
