ee7ef5c049cf962e83372f9bab9f5bf324b64251
tranphuongnamstudy <namtpce170126@fpt.edu.vn>
2025-03-23 21:14:14 +0700

    .

---
+++ Client/ViewModel/PostViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Client.ViewModel
+{
+    public class PostViewModel
+    {
+        public int AccountId { get; set; }
+        public string ContentPost { get; set; }
+        public string CategoryId { get; set; }
+        public List<IFormFile> Images { get; set; } // Để nhận nhiều ảnh từ form
+
+
+        public PostViewModel(int accountId, string contentPost, string categoryId, List<IFormFile> images)
+        {
+            AccountId = accountId;
+            this.ContentPost = contentPost;
+            this.CategoryId = categoryId;
+            Images = images;
+        }
+
+        public PostViewModel()
+        {
+        }
+    }
+}
