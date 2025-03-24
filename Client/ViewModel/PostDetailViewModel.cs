2a193858087302ca5c019fd69148b81abdf0a505
THUC <nht.071203@gmail.com>
2025-03-24 14:55:45 +0700

    post datail

---
+++ Client/ViewModel/PostDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Client.DTOs;
+using Client.Models;
+
+namespace Client.ViewModel
+{
+    public class PostDetailViewModel
+    {
+        public PostDTO PostDTO { get; set; }
+        public List<Comment> ListComment { get; set; } = new List<Comment>();
+        public Dictionary<int, Account> CommentAccounts { get; set; } = new Dictionary<int, Account>();
+        public int CountCommentPost { get; set; }
+        public int CountLikePost { get; set; }
+        public int CountSharePost { get; set; }
+        public bool IsLikedByUser { get; set; }
+        public int View { get; set; }
+        public string CommentContent { get; set; }
+    }
+}
